98f8ca0efea1f604ed37b9412961696220830d86
afedynitch <afedynitch@gmail.com>
2023-02-07 14:43:17 +0000

    Rewrite

--- src/daemonflux/flux.py
+++ src/daemonflux/flux.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pickle
 import pathlib
-from dataclasses import dataclass
 from utils import grid_cov, quantities
+from contextlib import contextmanager
 
 # # Anatoli's installation requires me to add this
 # import sys
@@ -17,27 +17,28 @@ def format_angle(ang):
     return "{:4.4f}".format(float(ang))
 
 
-@dataclass
 class FluxParameters:
-    known_parameters: list
-    parameter_values: np.ndarray
-    cov: np.ndarray
+    def __init__(
+        self, known_parameters: list, values: np.ndarray, cov: np.ndarray
+    ) -> None:
+        self.known_parameters = known_parameters
+        self.values = values
+        self.cov = cov
 
     @property
-    def cov_inv(self):
+    def invcov(self) -> np.ndarray:
         return np.linalg.inv(self.cov)
 
     @property
-    def param_errors(self):
+    def errors(self) -> np.ndarray:
         return np.sqrt(np.diag(self.cov))
 
     @property
-    def chi2(self, params={}):
-        return grid_cov(self.parameter_values, self.cov_inv)
-    
+    def chi2(self, params={}) -> np.ndarray:
+        return grid_cov(self.values, self.invcov)
+
     def __iter__(self):
-        for p, pv in zip(self.known_parameters, self.parameter_values):
-            print("Hello",p, pv)
+        for p, pv in zip(self.known_parameters, self.values):
             yield p, pv
 
 
@@ -65,9 +66,6 @@ class Flux:
         if not use_calibration:
             cal_file = None
 
-        self.param_defaults = {}
-        self.cov = None
-
         self._load_splines(spl_file, cal_file)
 
         self._apply_little_hacks()
@@ -88,27 +86,27 @@ class Flux:
         #     self.jac_spl["deis2"] = self.jac_spl["DEIS"]
 
     def _load_splines(self, spl_file, cal_file):
-        
+
         assert pathlib.Path(spl_file).is_file(), f"Spline file {spl_file} not found."
         (
-            self.known_params,
+            known_parameters,
             self.fl_spl,
             self.jac_spl,
             self.GSF19_cov,
         ) = pickle.load(open(spl_file, "rb"))
 
-        self.known_params = [
+        known_parameters = [
             k
-            for k in self.known_params
+            for k in known_parameters
             if k not in self.exclude or k.startswith("total_")
         ]
 
         if cal_file is None:
             # print("daemonflux calibration not used.")
             self.params = FluxParameters(
-                self.known_params,
-                np.zeros(len(self.known_params)),
-                np.diag(np.ones_like(self.known_params)),
+                known_parameters,
+                np.zeros(len(known_parameters)),
+                np.diag(np.ones(len(known_parameters))),
             )
             return
 
@@ -130,7 +128,7 @@ class Flux:
             return cov_new
 
         param_values = []
-        for ip, n in enumerate(self.known_params):
+        for ip, n in enumerate(known_parameters):
             try:
                 param_values = calibration_d["params"][n]["value"]
             except KeyError:
@@ -141,23 +139,23 @@ class Flux:
             [
                 (p, ip)
                 for ip, p in enumerate(calibration_d["cov_params"])
-                if p in self.known_params
+                if p in known_parameters
             ]
         )
         n_physics_params = max(original_param_order.values()) + 1
 
         assert sorted(original_param_order.keys()) == sorted(
-            self.known_params
+            known_parameters
         ), "Parameters inconsistent between spl and calibration file"
 
         # Create a new covariance with the correct order of parameters
         cov = _remap_covariance(
             original_param_order,
-            self.known_params,
+            known_parameters,
             calibration_d["cov_matrix"][:n_physics_params, :n_physics_params],
         )
-        for ip, pi in enumerate(self.known_params):
-            for jp, pj in enumerate(self.known_params):
+        for ip, pi in enumerate(known_parameters):
+            for jp, pj in enumerate(known_parameters):
                 assert (
                     calibration_d["cov_matrix"][
                         original_param_order[pi], original_param_order[pj]
@@ -170,44 +168,19 @@ class Flux:
                     + str(pj)
                     + " incorrectly sorted."
                 )
-        self.params = FluxParameters(self.known_params, np.asarray(param_values), cov)
+        self.params = FluxParameters(known_parameters, np.asarray(param_values), cov)
 
-    # def _set_new_parameters(self, params):
-
-    #     assert self.cov is not None, "Covariance needs to be defined."
-    #     # Get the individual errors on the parameters from cov
-    #     self.param_errors = np.sqrt(np.diag(self.cov))
-    #     # Invert the covariance matrix
-    #     self.cov_inv = np.linalg.inv(self.cov)
-
-    #     new_defaults = {}
-    #     for ip, onep in enumerate(self.known_params):
-    #         if onep in params:
-    #             new_params[onep] = (
-    #                 self.param_defaults[onep] + self.param_errors[ip] * params[onep]
-    #             )
-    #         else:
-    #             new_params[onep] = self.param_defaults[onep]
-
-    #     self.calculate_chi2(params)
-
-    #     return new_params
-
-    def _check_input(self, grid, exp_tag, angle, params, quantity):
+    def _check_input(self, grid, exp_tag, angle, quantity):
 
         assert self.fl_spl is not None, "Splines have to be initialized"
         assert self.jac_spl is not None, "Jacobians required for error estimate"
-        assert np.max(grid) < 1e9 and np.min(grid) >= 5e-2, "Energy out of range"
+        assert np.max(grid) <= 1e9 and np.min(grid) >= 5e-2, "Energy out of range"
         assert exp_tag in self.fl_spl, "Available experiments: {0}".format(
             ", ".join(self.fl_spl.keys())
         )
         assert angle in self.fl_spl[exp_tag], "Available angles: {0}".format(
             ", ".join(self.fl_spl[exp_tag].keys())
         )
-        for k in params:
-            assert k in self.known_params, "Correction parameter {0} not in {1}".format(
-                k, ", ".join(self.known_params)
-            )
         assert quantity in self._quantities, "Quantity must be one of {0}.".format(
             ", ".join(self._quantities)
         )
@@ -215,7 +188,55 @@ class Flux:
             sum(["GSF_" in p for p in self.exclude]) == 0
         ), "Individual GSF parameters can't be excluded, use 'GSF' globally."
 
-    def get_flux_new(
+    @contextmanager
+    def _temporary_parameters(self, modified_params: dict):
+        from copy import copy
+
+        if not modified_params:
+            yield
+        else:
+            prev = copy(self.params.values)
+            pars = self.params
+            for k in modified_params:
+                if k not in pars.known_parameters:
+                    raise KeyError(f"Cannot modify {k}, paramter unknown.")
+                par_idx = pars.known_parameters.index(k)
+                pars.values[par_idx] += pars.errors[par_idx] * modified_params[k]
+
+            yield
+            self.params.values = prev
+
+    @contextmanager
+    def _temporary_exclude_parameters(self, exclude_str):
+        from copy import deepcopy
+
+        if not exclude_str:
+            yield
+        else:
+            prev = deepcopy(self.params)
+            pars = self.params
+            new_kp = []
+            new_values = []
+            keep_cov = []
+            for ik, k in enumerate(pars.known_parameters):
+
+                if exclude_str in k:
+                    continue
+                new_kp.append(k)
+                new_values.append(pars.values[ik])
+                keep_cov.append(ik)
+
+            pars.cov = np.take(np.take(pars.cov, keep_cov, axis=0), keep_cov, axis=1)
+            pars.values = np.asarray(new_values)
+            pars.known_parameters = new_kp
+
+            assert pars.cov.shape[0] == len(pars.known_parameters) == len(pars.values)
+
+            yield
+
+            self.params = prev
+
+    def flux(
         self,
         grid,
         exp_tag,
@@ -224,84 +245,58 @@ class Flux:
         params={},
         make_checks=True,
     ):
-        angle = format_angle(zenith_deg)  # if zenith_deg != "average" else zenith_deg
+        angle = "average" if zenith_deg == "average" else format_angle(zenith_deg)
+
         if make_checks:
-            self._check_input(grid, exp_tag, angle, params, quantity)
-
-        # if params:
-        #     self._set_new_parameters(params)
-        # else:
-        #     self._reset_params()
-
-        # params = self.recast_params(params)
+            self._check_input(grid, exp_tag, angle, quantity)
 
         jac = self.jac_spl[exp_tag][angle]
         fl = self.fl_spl[exp_tag][angle]
-
-        corrections = 1 + np.sum(
-            [
-                v * jac[dk][quantity](np.log(grid))
-                for (dk, v) in self.params
-                if dk not in self.exclude
-            ],
-            axis=0,
-        )
+        with self._temporary_parameters(params):
+            corrections = 1 + np.sum(
+                [v * jac[dk][quantity](np.log(grid)) for (dk, v) in self.params],
+                axis=0,
+            )
         return np.exp(fl[quantity](np.log(grid))) * corrections
 
-    def get_error_new(
+    def error(
         self,
         grid,
         exp_tag,
         zenith_deg,
         quantity,
-        params={},
         make_checks=True,
-        include_GSF_in_total=True,
+        only_hadronic=False,
     ):
 
-        angle = format_angle(zenith_deg) if zenith_deg != "average" else zenith_deg
+        angle = "average" if zenith_deg == "average" else format_angle(zenith_deg)
+
         if make_checks:
-            self._check_input(grid, exp_tag, angle, params, quantity)
+            self._check_input(grid, exp_tag, angle, quantity)
 
-        if len(params) > 0:
-            sum_params = params
-        elif include_GSF_in_total:
-            sum_params = [p for p in self.known_params]
-        else:
-            sum_params = [p for p in self.known_params if "GSF" not in p]
+        with self._temporary_exclude_parameters("GSF" if only_hadronic else None):
+            print(self.params.known_parameters, self.params.values, self.params.errors)
 
-        jac = self.jac_spl[exp_tag][angle]
+            jac = self.jac_spl[exp_tag][angle]
 
-        jacfl = np.vstack(
-            [jac[par][quantity](np.log(grid)) for par in sum_params]
-        ).T
-        error = np.sqrt(np.diag(self._get_grid_cov(jacfl, self.cov)))
-        # if use_calibration:
-        # else:
-        error = np.sqrt(
-            np.sum(
-                [jac[dk][quantity](np.log(grid)) ** 2 for dk in sum_params],
-                axis=0,
-            )
-        )
-
-        if "GSF" not in self.exclude:
             jacfl = np.vstack(
-                # [jac[f"GSF_{i}"][quantity](np.log(grid)) for i in range(1, 7)]
-                # ).T
                 [
-                    jac["GSF_" + "%i" % i][quantity](np.log(grid))
-                    for i in range(1, 7)
+                    jac[par][quantity](np.log(grid))
+                    for par in self.params.known_parameters
                 ]
             ).T
-            gsf_error = np.sqrt(np.diag(self._get_grid_cov(jacfl, self.GSF19_cov)))
-            error = np.sqrt(error**2 + gsf_error**2)
-
-        return np.exp(self.fl_spl[exp_tag][angle][quantity](np.log(grid))) * error
+            error = np.sqrt(np.diag(self._get_grid_cov(jacfl, self.params.cov)))
+            error = np.sqrt(
+                np.sum(
+                    [
+                        jac[dk][quantity](np.log(grid)) ** 2
+                        for dk in self.params.known_parameters
+                    ],
+                    axis=0,
+                )
+            )
+            return np.exp(self.fl_spl[exp_tag][angle][quantity](np.log(grid))) * error
 
     def print_experiments(self):
-        # if self.fl_spl is None and self.jac_spl is None:
-        if self.fl_spl is None:
-            raise RuntimeError("Can not print experiments if flux spline unassigned.")
         for exp in self.fl_spl:
             print("{0}: [{1}]".format(exp, ", ".join(self.fl_spl[exp].keys())))
