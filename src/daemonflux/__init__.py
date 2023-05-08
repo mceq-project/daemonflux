69c02863912fa96f1de068beab87634b7ef74580
Anatoli Fedynitch <afedynitch@users.noreply.github.com>
2023-05-08 14:42:30 +0800

    Afedynitch/issue3 (#4)
    Closes #1

--- src/daemonflux/__init__.py
+++ src/daemonflux/__init__.py
@@ -1,5 +1,17 @@
+import sys
 from daemonflux.flux import Flux
 from importlib.metadata import version
+from scipy.version import version as scipy_version
+import scipy.interpolate  # noqa: F401
 
 __version__ = version("daemonflux")
 __all__ = ["Flux", "__version__"]
+
+if int(scipy_version.split(".")[1]) < 8:
+    sys.modules["scipy.interpolate._fitpack2"] = sys.modules[
+        "scipy.interpolate.fitpack2"
+    ]
+    raise DeprecationWarning(
+        "scipy version must be >= 1.8.0. "
+        + "Support for lower version will be removed in future releases."
+    )
