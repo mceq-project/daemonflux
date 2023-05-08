8394484e5a23b002b06dc2fed1e05fe6d5426f29
afedynitch <afedynitch@gmail.com>
2023-05-08 14:30:17 +0800

    Added workaround for scipy < 1.8

--- src/daemonflux/__init__.py
+++ src/daemonflux/__init__.py
@@ -1,5 +1,16 @@
 from daemonflux.flux import Flux
 from importlib.metadata import version
+from scipy.version import version as scipy_version
+import sys
 
 __version__ = version("daemonflux")
 __all__ = ["Flux", "__version__"]
+
+if int(scipy_version.split(".")[2]) < 8:
+    sys.modules["scipy.interpolate._fitpack2"] = sys.modules[
+        "scipy.interpolate.fitpack2"
+    ]
+    raise DeprecationWarning(
+        "scipy version must be >= 1.8.0. "
+        + "Support for lower version will be removed in future releases."
+    )
