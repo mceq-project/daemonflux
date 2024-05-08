b1828e24b9c84d505e25ab0fc184162f16fae863
afedynitch <afedynitch@gmail.com>
2024-05-08 10:53:04 +0000

    Energy argument can be a float

--- src/daemonflux/flux.py
+++ src/daemonflux/flux.py
@@ -381,16 +381,16 @@ class Flux:
         """
         return self._get_flux_instance(exp)._params
 
-    def flux(self, grid, zenith_deg, quantity, params={}, exp=""):
+    def flux(self, energy, zenith_deg, quantity, params={}, exp=""):
         """
-        The flux of a given quantity for the specified energy grid and zenith angles.
+        The flux of a given quantity for the specified energy energy and zenith angles.
 
         The flux is multiplied by E^3 in units of GeV^2/(cm^2 s sr).
 
         Parameters
         ----------
-        grid : np.ndarray
-            The energy grid in units of GeV.
+        energy : float or np.ndarray
+            The energy energy in units of GeV.
         zenith_deg : float or str or list of float
             The zenith angle in degrees. If "average", the average flux over all
             zenith angles will be returned.
@@ -409,18 +409,18 @@ class Flux:
         Exception
             If `zenith_deg` is "average" but splines do not contain average flux.
         """
-        return self._get_flux_instance(exp).flux(grid, zenith_deg, quantity, params)
+        return self._get_flux_instance(exp).flux(energy, zenith_deg, quantity, params)
 
-    def error(self, grid, zenith_deg, quantity, only_hadronic=False, exp=""):
+    def error(self, energy, zenith_deg, quantity, only_hadronic=False, exp=""):
         """
-        The flux of a given quantity for the specified energy grid and zenith angles.
+        The flux of a given quantity for the specified energy energy and zenith angles.
 
         The error is multiplied by E^3 in units of GeV^2/(cm^2 s sr).
 
         Parameters
         ----------
-        grid : np.ndarray
-            The energy grid for which to compute the error.
+        energy : float or np.ndarray
+            The energy energy for which to compute the error.
         zenith_deg : float or str or list of float
             The zenith angle in degrees, or a list of zenith angles.
         quantity : str
@@ -440,7 +440,7 @@ class Flux:
             If `zenith_deg` is "average" but splines do not contain average flux.
         """
         return self._get_flux_instance(exp).error(
-            grid,
+            energy,
             zenith_deg,
             quantity,
             only_hadronic,
@@ -574,30 +574,30 @@ class _FluxEntry(Flux):
             self._params = prev
             assert pars.cov.shape[0] == len(pars.known_parameters) == len(pars.values)
 
-    def _check_input(self, grid: np.ndarray, quantity: str) -> None:
+    def _check_input(self, energy: Union[np.ndarray, float], quantity: str) -> None:
         """
-        Check the validity of the input grid and quantity.
+        Check the validity of the input energy and quantity.
 
         Parameters
         ----------
-        grid : np.ndarray
-            The energy grid.
+        energy : float or np.ndarray
+            The energy energy.
         quantity : str
             The quantity to be calculated.
 
         Raises
         ------
         AssertionError
-            If the energy grid is out of range or if the quantity is unknown.
+            If the energy energy is out of range or if the quantity is unknown.
         """
-        assert np.max(grid) <= 1e9 and np.min(grid) >= 5e-2, "Energy out of range"
+        assert np.max(energy) <= 1e9 and np.min(energy) >= 5e-2, "Energy out of range"
         assert quantity in self._quantities, "Quantity must be one of {0}.".format(
             ", ".join(self._quantities)
         )
 
     def _flux_from_spl(
         self,
-        grid: np.ndarray,
+        energy: Union[np.ndarray, float],
         zenith_deg: str,
         quantity: str,
         params: Dict[str, float],
@@ -607,7 +607,7 @@ class _FluxEntry(Flux):
 
         Parameters
         ----------
-        grid : numpy.ndarray
+        energy : float or numpy.ndarray
             The grid of energies for which the flux is calculated.
         zenith_deg : str
             The zenith angle in degrees for which the flux is calculated.
@@ -620,28 +620,29 @@ class _FluxEntry(Flux):
         fl = self._fl_spl[zenith_deg]
         with self._temporary_parameters(params):
             corrections = 1 + np.sum(
-                [v * jac[dk][quantity](np.log(grid)) for (dk, v) in self._params],
+                [v * jac[dk][quantity](np.log(energy)) for (dk, v) in self._params],
                 axis=0,
             )
-        return np.exp(fl[quantity](np.log(grid))) * corrections
+        return (np.exp(fl[quantity](np.log(energy))) * corrections).squeeze()
 
     def _flux_from_interp(
         self,
-        grid: np.ndarray,
+        energy: Union[np.ndarray, float],
         zenith_deg: Union[float, str, np.ndarray],
         quantity: str,
         params: Dict[str, float],
     ) -> np.ndarray:
         from scipy.interpolate import interp1d
 
+        energy = np.atleast_1d(energy).astype("float64")
         zenith_darr = np.atleast_1d(zenith_deg).astype("float64")
         zenith_carr = np.cos(np.deg2rad(zenith_darr))
 
         idxmin, idxmax = self._interpolation_domain(zenith_darr)
-        interp_array = np.zeros((idxmax - idxmin, len(grid)))
+        interp_array = np.zeros((idxmax - idxmin, len(energy)))
         for i, idx in enumerate(range(idxmin, idxmax)):
             interp_array[i, :] = self._flux_from_spl(
-                grid, self.zenith_angles[idx], quantity, params
+                energy, self.zenith_angles[idx], quantity, params
             )
         return interp1d(self._zenith_cos_arr[idxmin:idxmax], interp_array, axis=0)(
             zenith_carr
@@ -649,7 +650,7 @@ class _FluxEntry(Flux):
 
     def _error_from_spl(
         self,
-        grid: np.ndarray,
+        energy: Union[np.ndarray, float],
         zenith_deg: Union[float, str, np.ndarray],
         quantity: str,
         only_hadronic: bool,
@@ -665,30 +666,33 @@ class _FluxEntry(Flux):
 
             jacfl = np.vstack(
                 [
-                    jac[par][quantity](np.log(grid))
+                    jac[par][quantity](np.log(energy))
                     for par in self._params.known_parameters
                 ]
             ).T
             error = np.sqrt(np.diag(grid_cov(jacfl, self._params.cov)))
-            return np.exp(self._fl_spl[zenith_deg][quantity](np.log(grid))) * error
+            return (
+                np.exp(self._fl_spl[zenith_deg][quantity](np.log(energy))) * error
+            ).squeeze()
 
     def _error_from_interp(
         self,
-        grid: np.ndarray,
+        energy: Union[np.ndarray, float],
         zenith_deg: Union[float, str, np.ndarray],
         quantity: str,
         only_hadronic: bool,
     ) -> np.ndarray:
         from scipy.interpolate import interp1d
 
+        energy = np.atleast_1d(energy).astype("float64")
         zenith_darr = np.atleast_1d(zenith_deg).astype("float64")
         zenith_carr = np.cos(np.deg2rad(zenith_darr))
 
         idxmin, idxmax = self._interpolation_domain(zenith_darr)
-        interp_array = np.zeros((idxmax - idxmin, len(grid)))
+        interp_array = np.zeros((idxmax - idxmin, len(energy)))
         for i, idx in enumerate(range(idxmin, idxmax)):
             interp_array[i, :] = self._error_from_spl(
-                grid, self.zenith_angles[idx], quantity, only_hadronic
+                energy, self.zenith_angles[idx], quantity, only_hadronic
             )
         return interp1d(self._zenith_cos_arr[idxmin:idxmax], interp_array, axis=0)(
             zenith_carr
@@ -737,18 +741,18 @@ class _FluxEntry(Flux):
 
     def flux(
         self,
-        grid: np.ndarray,
+        energy: Union[float, np.ndarray],
         zenith_deg: Union[float, str, np.ndarray],
         quantity: str,
         params: Dict[str, float] = {},
-    ) -> np.ndarray:
+    ) -> Union[float, np.ndarray]:
         """
-        Compute the flux at the given energy grid, zenith angle, and quantity.
+        Compute the flux at the given energy energy, zenith angle, and quantity.
 
         Parameters
         ----------
-        grid : np.ndarray
-            The energy grid in units of GeV.
+        energy : float or np.ndarray
+            The energy energy in units of GeV.
         zenith_deg : float or str or list of float
             The zenith angle in degrees. If "average", the average flux over all
             zenith angles will be returned.
@@ -759,7 +763,7 @@ class _FluxEntry(Flux):
 
         Returns
         -------
-        np.ndarray
+        float or np.ndarray
             The flux multiplied by E^3 in units of GeV^2/(cm^2 s sr).
 
         Raises
@@ -767,28 +771,28 @@ class _FluxEntry(Flux):
         Exception
             If `zenith_deg` is "average" but splines do not contain average flux.
         """
-        self._check_input(grid, quantity)
+        self._check_input(energy, quantity)
         # handle the case where the zenith angle is "average"
         if isinstance(zenith_deg, str) and zenith_deg == "average":
             if not self._spl_contains_average:
                 raise Exception("Splines do not contain average flux")
-            return self._flux_from_spl(grid, zenith_deg, quantity, params)
+            return self._flux_from_spl(energy, zenith_deg, quantity, params)
 
         # handle the case where the zenith angle is a single value or an array
         if not is_iterable(zenith_deg) and float(zenith_deg) in self._zenith_deg_arr:
             return self._flux_from_spl(
-                grid, format_angle(float(zenith_deg)), quantity, params
+                energy, format_angle(float(zenith_deg)), quantity, params
             )
         else:
-            return self._flux_from_interp(grid, zenith_deg, quantity, params)
+            return self._flux_from_interp(energy, zenith_deg, quantity, params)
 
     def error(
         self,
-        grid: np.ndarray,
+        energy: Union[float, np.ndarray],
         zenith_deg: Union[float, str],
         quantity: str,
         only_hadronic: bool = False,
-    ) -> np.ndarray:
+    ) -> Union[float, np.ndarray]:
         """
         Return the error of the flux estimation for the given parameters.
 
@@ -796,8 +800,8 @@ class _FluxEntry(Flux):
 
         Parameters
         ----------
-        grid : np.ndarray
-            The energy grid for which to compute the error.
+        energy : float or np.ndarray
+            The energy energy for which to compute the error.
         zenith_deg : float or str
             The zenith angle in degrees. If a string, it must be "average".
         quantity : str
@@ -808,7 +812,7 @@ class _FluxEntry(Flux):
 
         Returns
         -------
-        np.ndarray
+        float or np.ndarray
             The error of the flux estimation.
 
         Raises
@@ -816,21 +820,21 @@ class _FluxEntry(Flux):
         Exception
             If `zenith_deg` is "average" but splines do not contain average flux.
         """
-        self._check_input(grid, quantity)
+        self._check_input(energy, quantity)
 
         # handle the case where the zenith angle is "average"
         if isinstance(zenith_deg, str) and zenith_deg == "average":
             if not self._spl_contains_average:
                 raise Exception("Splines do not contain average flux")
-            return self._error_from_spl(grid, zenith_deg, quantity, only_hadronic)
+            return self._error_from_spl(energy, zenith_deg, quantity, only_hadronic)
 
         # handle the case where the zenith angle is a single value or an array
         if not is_iterable(zenith_deg) and float(zenith_deg) in self._zenith_deg_arr:
             return self._error_from_spl(
-                grid, format_angle(zenith_deg), quantity, only_hadronic
+                energy, format_angle(zenith_deg), quantity, only_hadronic
             )
         else:
-            return self._error_from_interp(grid, zenith_deg, quantity, only_hadronic)
+            return self._error_from_interp(energy, zenith_deg, quantity, only_hadronic)
 
     def chi2(self, params={}):
         """
